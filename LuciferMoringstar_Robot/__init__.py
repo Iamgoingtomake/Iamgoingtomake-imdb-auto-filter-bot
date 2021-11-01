47f54bd1b361096ad3319f61e8bb710bd11d28f4
Iamgoingtomake <89531655+Iamgoingtomake@users.noreply.github.com>
2021-11-01 12:48:05 +0530

    Update __init__.py

--- LuciferMoringstar_Robot/__init__.py
+++ LuciferMoringstar_Robot/__init__.py
@@ -30,8 +30,8 @@ Bot Owner Only
 ABOUT = """
 ➥ Name :  Auto Filter V3
     
-➥ Creator : [Naughty PROFESSOR](
-https://youtube.com/c/NaughtyPROFESSOR)
+➥ Creator : [Hyetelegram Bots ](
+https://t.me/HyetelegramBots)
 
 ➥ Language : Python3
 
